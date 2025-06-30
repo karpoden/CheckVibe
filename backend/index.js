df7bec41b2767176e6dd77830e37dcdc6c0475cd
karpoden <dan.karpov98@gmail.com>
2025-06-30 21:05:11 +0500

    deploy fix host ver 0.3.2

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,6 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
 });
