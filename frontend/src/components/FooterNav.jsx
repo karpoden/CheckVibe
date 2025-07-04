ce97a12bc37c6bd6b0ab63509e01902c40c26768
karpoden <dan.karpov98@gmail.com>
2025-07-04 16:05:12 +0500

    fix color and size buttons ver 0.6.9

--- frontend/src/components/FooterNav.jsx
+++ frontend/src/components/FooterNav.jsx
@@ -21,15 +21,15 @@ export default function FooterNav() {
     padding: "10px",
     borderRadius: "12px",
     fontWeight: "bold",
-    fontSize: "1.1em",
+    fontSize: "1.25em",
     cursor: "pointer",
     transition: "box-shadow 0.2s, background 0.2s",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     // flexDirection: "column", // Чтобы иконка и текст шли друг под другом, убери если нужно в линию
-    width: "75px", // Фиксированная ширина
-    height: "40px", // Фиксированная высота
+    // width: "75px", // Фиксированная ширина
+    // height: "40px", // Фиксированная высота
     textDecoration: "none",
     textAlign: "center",
   });
@@ -41,7 +41,7 @@ export default function FooterNav() {
       alignItems: "center",
       gap: "20px",
       padding: "10px",
-      backgroundColor: "#1a1a1a",
+      // backgroundColor: "#1a1a1a",
     }}>
       <Link to="/" style={navBtnStyle(pathname === "/")} className={linkClass('/')}>
         <Home size={24} />
