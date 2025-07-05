ceec5bb5d9531df53ca93792635d754234a02566
karpoden <dan.karpov98@gmail.com>
2025-07-05 14:44:59 +0500

    stabel ver 0.7.1

--- frontend/src/components/RandomPlayer.jsx
+++ frontend/src/components/RandomPlayer.jsx
@@ -220,7 +220,7 @@ export default function RandomPlayer() {
               ref={cardRef}
               onSwipe={handleSwipe}
               preventSwipe={['up', 'down']}
-              swipeThreshold={30}
+              swipeThreshold={150}
             >
               <div
                 style={{
